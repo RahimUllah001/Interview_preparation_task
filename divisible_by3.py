c1c8a1913d264b3b7bf45f0b69f76e8bffa3081a
Rahim <rahimullah25678@gmail.com>
2024-11-05 12:51:58 +0500

    Project completed

--- divisible_by3.py
+++ divisible_by3.py
@@ -1,35 +1,39 @@
 # task one method 1
-def check_num(num):
-    if num%3 == 0:
-        print(num," divisible by 3")
-    else:
-        print(num," not divisible by 3")
+# def check_num(num):
+#     if num%3 == 0:
+#         print(num," divisible by 3")
+#     else:
+#         print(num," not divisible by 3")
 
-check_num(91)
+# check_num(91)
 
 
-# task 1 method 2 
+# # task 1 method 2 
 
-def check_number(num):
-    num1 = num
-    digit_sum = 0
-    while num > 0:
-        rem = num % 10
-        num = num//10
-        digit_sum += rem
+# def check_number(num):
+#     num1 = num
+#     digit_sum = 0
+#     while num > 0:
+#         rem = num % 10
+#         num = num//10
+#         digit_sum += rem
 
-    if digit_sum % 3 == 0:
-        print(num1," divisible by 3")
-    else:
-        print(num1," not divisible by 3")
-check_number(13)
+#     if digit_sum % 3 == 0:
+#         print(num1," divisible by 3")
+#     else:
+#         print(num1," not divisible by 3")
+# check_number(13)
 
 # task 1 method 3
 
-def checkNumber(num):
-    digitsum = sum(map(int,str(num)))
-    digitsum1 = list(map(int,str(num)))
-    
-    print(digitsum1)
+# def checkNumber(num):
+#     # digitsum = sum(map(int,str(num)))
+#     # digitsum1 = list(map(int,str(num)))
+#     # value_if_true if condition else value_if_false
+#     print(num,"divisible" if  sum(map(int, str(num))) % 3 == 0 else"not divisible")
+# checkNumber(123)
 
-checkNumber(1234)
+# task 1 method 3
+def checkNumber(num):
+    print(num,"divisible" if  sum(map(int, str(num))) % 3 == 0 else"not divisible")
+checkNumber(123)
